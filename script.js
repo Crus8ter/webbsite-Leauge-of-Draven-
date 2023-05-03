85bbf71c359c4e8a3e72c54e48a5d823ac4efc06
Crus8ter <isakh485@gmail.com>
2023-05-03 19:59:57 +0200

    something cool

--- script.js
+++ script.js
@@ -8,5 +8,29 @@ function openNav() {
   }
 
   function openAbilityQ() {
-    document.getElementById("AbilityQ").style.width = "300px";
+    document.getElementById("AbilityQ").style.zIndex = "1";
+    document.getElementById("AbilityW").style.zIndex = "0";
+    document.getElementById("AbilityE").style.zIndex = "0";
+    document.getElementById("AbilityR").style.zIndex = "0";
+  }
+
+  function openAbilityW() {
+    document.getElementById("AbilityQ").style.zIndex = "0";
+    document.getElementById("AbilityW").style.zIndex = "1";
+    document.getElementById("AbilityE").style.zIndex = "0";
+    document.getElementById("AbilityR").style.zIndex = "0";
+  }
+
+  function openAbilityE() {
+    document.getElementById("AbilityQ").style.zIndex = "0";
+    document.getElementById("AbilityW").style.zIndex = "0";
+    document.getElementById("AbilityE").style.zIndex = "1";
+    document.getElementById("AbilityR").style.zIndex = "0";
+  }
+
+  function openAbilityR() {
+    document.getElementById("AbilityQ").style.zIndex = "0";
+    document.getElementById("AbilityW").style.zIndex = "0";
+    document.getElementById("AbilityE").style.zIndex = "0";
+    document.getElementById("AbilityR").style.zIndex = "1";
   }
