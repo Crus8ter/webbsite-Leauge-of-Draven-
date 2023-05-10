27e906a5472689545ef7bb68ee1e468579b9bb6a
crus8ter <isakh485@gmail.com>
2023-05-10 13:14:25 +0200

    Merge branch 'main' of https://github.com/Crus8ter/webbsite-Leauge-of-Draven-

---
+++ video.js
@@ -0,0 +1,15 @@
+document.getElementById("ability-Q").addEventListener("click", function() {
+    document.getElementById("video").src = "https://d28xe8vt774jo5.cloudfront.net/champion-abilities/0119/ability_0119_Q1.webmhttps://d28xe8vt774jo5.cloudfront.net/champion-abilities/0119/ability_0119_Q1.webm";
+})
+
+document.getElementById("ability-W").addEventListener("click", function() {
+    document.getElementById("video").src = "https://d28xe8vt774jo5.cloudfront.net/champion-abilities/0119/ability_0119_W1.webm";
+})
+
+document.getElementById("ability-E").addEventListener("click", function() {
+    document.getElementById("video").src = "https://d28xe8vt774jo5.cloudfront.net/champion-abilities/0119/ability_0119_E1.mp4";
+})
+
+document.getElementById("ability-R").addEventListener("click", function() {
+    document.getElementById("video").src = "https://d28xe8vt774jo5.cloudfront.net/champion-abilities/0119/ability_0119_R1.webm";
+})
